1545389eea92d6125f526fd5741da7bb293646ac
d36choi <yuio1126@gmail.com>
2021-03-11 15:07:40 +0900

    Initial commit

---
+++ src/main/java/com/example/awssnslistener/controller/SnsController.java
@@ -0,0 +1,29 @@
+package com.example.awssnslistener.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.aws.messaging.config.annotation.NotificationMessage;
+import org.springframework.cloud.aws.messaging.config.annotation.NotificationSubject;
+import org.springframework.cloud.aws.messaging.endpoint.NotificationStatus;
+import org.springframework.cloud.aws.messaging.endpoint.annotation.NotificationMessageMapping;
+import org.springframework.cloud.aws.messaging.endpoint.annotation.NotificationSubscriptionMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/endpoint")
+public class SnsController {
+
+    @NotificationMessageMapping
+    public void receiveNotification(@NotificationMessage String message,
+                                    @NotificationSubject String subject) {
+
+        log.info(String.format("message : %s \nsubject : %s",message,subject));
+    }
+
+    @NotificationSubscriptionMapping
+    public void confirmSubscription(NotificationStatus notificationStatus) {
+        notificationStatus.confirmSubscription();
+    }
+
+}
---
+++ src/test/java/com/example/awssnslistener/AwssnsListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awssnslistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwssnsListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
